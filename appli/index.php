a737606aae4d4c1507dda9f753974850012747ff
Alexandre Leote <alexandreleoteweb@gmail.com>
2024-11-04 16:54:33 +0100

    Correction to index and recap

--- appli/index.php
+++ appli/index.php
@@ -5,14 +5,38 @@
 
 <!-- FORM -->
 
-        <div class="d-flex flex-column w-75 flex-grow-1">
-            <div class="d-flex justify-content-center flex-column flex-wrap ml-5 mr-5 mb-5">
-
-                <form action="traitement.php?action=ajoutProduit" method="post" enctype="multipart/form-data" class="d-flex">
-                    <h1 class="text-center text-info display-2 user-select-none"><u>Ajouter un produit</u></h1>    
-                </form>
-            </div>
-        </div>
+<div class="d-flex flex-column w-75 flex-grow-1">
+    <div class="d-flex justify-content-center flex-column flex-wrap ml-5 mr-5 mb-5">
+            
+        <form action="traitement.php?action=ajoutProduit" method="post" enctype="multipart/form-data" class="d-flex">
+            <h1 class="text-center text-info display-2 user-select-none"><u>Ajouter un produit</u></h1> 
+                
+        </form>
+            <form action="traitement.php" method="post">
+            <p>
+                <label>
+                    Nom du produit :
+                    <input type="text" name="name">
+                </label>
+            </p>
+            <p>
+                <label>
+                    Prix du produit :
+                    <input type="number" step="any" name="price">
+                </label>
+            </p>
+            <p>
+                <label>
+                    Quantité désirée :
+                    <input type="number" name="qtt" value="1">
+                </label>
+            </p>
+            <p>
+                <input type="submit" name="submit" value="Ajouter le produit">
+            </p>
+        </form>
+    </div>
+</div>
 
 <?php
 
--- appli/recap.php
+++ appli/recap.php
@@ -1,53 +1,45 @@
 <?php
     session_start();
+    ob_start();
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   
-    <!-- BOOTSTRAP CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icon@1.5.0/font/bootstrap-icons.css">
-    <title>Récapitulatif des produits</title>
-</head>
-<body>
-    <?php
-        if(!isset($_SESSION['products']) || empty($_SESSION{'products'})) {
-            echo "<p>Aucun produit en session...</p>";
-        }
-        else {
-            echo "<table class='table'>",
-                    "<thead>",
-                        "<tr>",
-                            "<th>#</th>",
-                            "<th>Nom</th>",
-                            "<th>Prix</th>",
-                            "<th>Quantité</th>",
-                            "<th>Total</th>",
-                        "</tr>",
-                    "</thead>",
-                    "<tbody>";
-            $totalGeneral = 0;
-            foreach($_SESSION['products'] as $index => $product) {
-                echo "<tr>",
-                        "<td>".$index."</td>",
-                        "<td>".htmlspecialchars($product['name'], ENT_QUOTES, "UTF-8")."</td>",
-                        "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€</td>",
-                        "<td>".$product['qtt']."</td>",
-                        "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
-                    "</tr>";
-                $totalGeneral += $product['total'];
-            }
+
+<?php
+    if(!isset($_SESSION['products']) || empty($_SESSION{'products'})) {
+        echo "<p>Aucun produit en session...</p>";
+    }
+    else {
+        echo "<table class='table'>",
+                "<thead>",
+                    "<tr>",
+                        "<th>#</th>",
+                        "<th>Nom</th>",
+                        "<th>Prix</th>",
+                        "<th>Quantité</th>",
+                        "<th>Total</th>",
+                    "</tr>",
+                "</thead>",
+                "<tbody>";
+        $totalGeneral = 0;
+        foreach($_SESSION['products'] as $index => $product) {
             echo "<tr>",
-                    "<td colspan=4>Total général : </td>",
-                    "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
-                 "</tr>",
-                "</tbody>",
-            "</table>";
+                    "<td>".$index."</td>",
+                    "<td>".htmlspecialchars($product['name'], ENT_QUOTES, "UTF-8")."</td>",
+                    "<td>".number_format($product['price'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                    "<td>".$product['qtt']."</td>",
+                    "<td>".number_format($product['total'], 2, ",", "&nbsp;")."&nbsp;€</td>",
+                "</tr>";
+            $totalGeneral += $product['total'];
         }
-    ?>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-</body>
-</html>
+        echo "<tr>",
+                "<td colspan=4>Total général : </td>",
+                "<td><strong>".number_format($totalGeneral, 2, ",", "&nbsp;")."&nbsp;€</strong></td>",
+                "</tr>",
+            "</tbody>",
+        "</table>";
+    }
+?>
+
+<?php
+    $content = ob_get_clean();
+
+    require_once "template.php"; ?>
