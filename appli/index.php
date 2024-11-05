a6adacd098f257795b37fe43fa72bcdb15a73caf
Alexandre Leote <alexandreleoteweb@gmail.com>
2024-11-05 12:05:36 +0100

    Added swittch case "add" with $_GET and $_POST

--- appli/index.php
+++ appli/index.php
@@ -9,11 +9,10 @@
 <div class="d-flex flex-column w-75 flex-grow-1">
     <div class="d-flex justify-content-center flex-column flex-wrap ml-5 mr-5 mb-5">
             
-        <form action="traitement.php?action=ajoutProduit" method="post" enctype="multipart/form-data" class="d-flex">
+        <form action="traitement.php?action=add" method="post" enctype="multipart/form-data" class="d-flex">
+            <div class="p-2">
             <h1 class="text-center text-info display-2 user-select-none"><u>Ajouter un produit</u></h1> 
-                
-        </form>
-            <form action="traitement.php?action=add" method="post">
+               
             <p>
                 <label>
                     Nom du produit :
@@ -35,6 +34,7 @@
             <p>
                 <input type="submit" name="submit" value="Ajouter le produit">
             </p>
+            </div>  
         </form>
     </div>
 </div>
--- appli/traitement.php
+++ appli/traitement.php
@@ -1,20 +1,35 @@
 <?php
     session_start();
 
-    if(isset($_POST['submit'])) {
-        $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-        $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
-        $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+    if(isset($_GET['action'])) {
 
-        if($name && $price && $qtt) {
-            $product = [
-                "name" => $name,
-                "price" => $price,
-                "qtt" => $qtt,
-                "total" => $price*$qtt
-            ];
-
-            $_SESSION['products'][] = $product;
+        switch($_GET['action']) {
+            case 'add' :
+                if(isset($_POST['submit'])) {
+                    $name = filter_input(INPUT_POST, "name", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+                    $price = filter_input(INPUT_POST, "price", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
+                    $qtt = filter_input(INPUT_POST, "qtt", FILTER_VALIDATE_INT);
+                
+                    if($name && $price && $qtt) {
+                        $product = [
+                            "name" => $name,
+                            "price" => $price,
+                            "qtt" => $qtt,
+                            "total" => $price*$qtt
+                        ];
+                
+                        $_SESSION['products'][] = $product;
+                    }
+                }
+            break;
+            case 'delete' :
+            break;
+            case 'clear' :
+            break;
+            case 'up-qtt' :
+            break;
+            case 'down-qtt' :
+            break;
         }
     }
 
